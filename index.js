63169bd29b7a9a4028094349264924b100bde9be
yaba010 <152669041+yaba010@users.noreply.github.com>
2023-12-19 17:45:05 +0800

    更新 index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-document.addEventListener('keydown', function(event) {
-            if (event.ctrlKey && event.shiftKey && event.key === 'F') {
-                event.preventDefault(); 
-                var sourceCode = document.documentElement.innerHTML;
-                sourceCode = sourceCode.replace(/./g, ' '); 
-                alert('源代码已加密，显示为乱码:\n\n' + sourceCode);
-            }
-        });
+// 混淆和加密源代码function obfuscateSourceCode(sourceCode) {
+  // 将源代码转换为字符串
+  const sourceCodeString = sourceCode.toString();
+  // 创建一个混淆和加密的函数
+  const obfuscatedCode = `
+    (function() {
+      var originalCode = "${sourceCodeString}";
+      var obfuscatedCode = "";
+      var charCode = originalCode.charCodeAt(0);
+      while (charCode > 0) {
+        var randomCharCode = Math.floor(Math.random() * 100) + 1;
+        obfuscatedCode += String.fromCharCode(charCode % randomCharCode);
+        charCode = Math.floor(charCode / randomCharCode);
+      }
+      eval(obfuscatedCode);
+    })();
+  `;
+  // 返回混淆和加密后的函数
+  return obfuscatedCode;
+}
+// 将源代码混淆和加密并插入到页面中const obfuscatedCode = obfuscateSourceCode(document.documentElement.innerHTML);
+document.documentElement.innerHTML = obfuscatedCode;
